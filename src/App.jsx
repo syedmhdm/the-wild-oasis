c322b1d16a9e9cc5f8ee0d7e9bb71e49fbb4dc42
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-12-24 14:45:46 +0530

    reausable defaultProps styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 function App() {
   const StyledApp = styled.main`
-    background-color: orangered;
+    /* background-color: orangered; */
     padding: 20px;
   `;
 
@@ -14,12 +15,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as='h1'>The Wild Oasis</Heading>
-        <Heading as='h2'>Check in and out</Heading>
-        <Button>Check In</Button>
-        <Button>Check Out</Button>
-        <Heading as='h3'>Form</Heading>
-        <Input type='number' placeholder='Number of guests' />
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button>Check In</Button>
+              <Button variation='secondary' size='small'>
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='number' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );

9a03012caf4d325bd2861de89bb1a874d57e479d
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-03 03:54:27 +0530

    booking page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
+import Booking from "./pages/Booking";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import GloabalStyles from "./styles/GlobalStyles";
@@ -33,6 +34,7 @@ function App() {
             <Route index element={<Navigate replace to={"/dashboard"} />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='bookings' element={<Bookings />} />
+            <Route path='bookings/:bookingId' element={<Booking />} />
             <Route path='cabins' element={<Cabins />} />
             <Route path='users' element={<Users />} />
             <Route path='settings' element={<Settings />} />

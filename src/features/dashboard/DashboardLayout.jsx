fbb9b6446da7b8663b48fe2c9c4f1a067c4fe7b8
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-04 01:54:30 +0530

    dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Today's activity</div>
+      <div>Statistics</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
